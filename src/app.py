58d048e6e4159353993dfe0d305635cc32ce9158
Alfonso Caro Portillo <alfonsocarop@gmail.com>
2021-10-13 08:46:56 -0500

    :sparkles: Git inicial code for backend

---
+++ src/app.py
@@ -0,0 +1,72 @@
+import os
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['TESTING'] = True
+app.config['FLASK_ENV'] = 'development'
+app.config['SECRET_KEY'] = 'GDtfDCFYjD'
+app.config['DEBUG'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+db = SQLAlchemy(app)
+
+from models import *
+
+db.create_all()
+
+#Index
+@app.route("/")
+def index():
+    return "This is the app index"
+
+#Add client and money
+@app.route("/add")
+def add_client():
+    name=request.args.get('name')
+    money=request.args.get('money')
+    try:
+        client=Client(
+            name=name,
+            money=money
+        )
+        db.session.add(client)
+        db.session.commit()
+        return "Client added with id={}".format(client.id)
+    except Exception as e:
+	    return(str(e))
+
+#Get all clients
+@app.route("/getall")
+def get_all():
+    try:
+        clients=Client.query.all()
+        return  jsonify([e.serialize() for e in clients])
+    except Exception as e:
+	    return(str(e))
+
+#Get client by ID
+@app.route("/get/<id_>")
+def get_by_id(id_):
+    try:
+        client=Client.query.filter_by(id=id_).first()
+        return jsonify(client.serialize())
+    except Exception as e:
+	    return(str(e))
+
+#Get client by Name
+@app.route("/getn/<name_>")
+def get_by_name(name_):
+    try:
+        client=Client.query.filter_by(name=name_).first()
+        return jsonify(client.serialize())
+    except Exception as e:
+	    return(str(e))
+
+if __name__ == '__main__':
+    app.run()
